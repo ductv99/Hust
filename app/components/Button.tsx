4b951fad1d91f0d8c3568ff6f00b2f1753673345
Van Duc Tran <vanduc.tran@wiinte.com>
2024-02-16 01:14:08 +0700

    create add product to cart

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -24,6 +24,7 @@ const Button: React.FC<ButtonProps> = (
     }) => {
     return (
         <button disabled={disabled}
+            onClick={onClick}
             className={`disabled:opacity-70 disabled:cursor-not-allowed rounded-md hover:opacity-80 transition w-full border-slate-700 flex items-center justify-center gap-2 
             ${outline ? "bg-white" : "bg-slate-700"}
             ${outline ? "text-slate-700" : "text-white"}
